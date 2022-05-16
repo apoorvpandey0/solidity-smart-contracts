ef77898a76473a7f5e30954d9e134061f25290e6
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-05-16 18:38:14 +0530

    completed brownie fund me

---
+++ brownie fund me/src/scripts/deploy.py
@@ -0,0 +1,36 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    # If chain is not local then use original price feed address
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    
+    # Else deploy mock price feed and then use its address
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+
+        # Publishing source code status is to be fetched acc to the deloyment local/testnet
+        # The verify property has been defined in config.yaml file
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
